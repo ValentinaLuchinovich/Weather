ab4589c5104f69ab69e354676f3ee184c1c6ad9e
ValentinaLuchinovich <valinkontakt11@bk.ru>
2022-01-11 21:06:50 +0300

    Обновление интерфейса приложения

--- Weather/Model/CurrentWeather.swift
+++ Weather/Model/CurrentWeather.swift
@@ -11,13 +11,35 @@ struct CurrentWeather {
     let cityName: String
     let temperature: Double
     var temperatureString: String {
-        return "\(temperature.rounded())"
+        // Округляем значение температуры
+        // Цифра после знака % указывает кол-во знаков после запятой
+        return String(format: "%.0f", temperature)
     }
     let feelsLiketemperature: Double
     var feelsLiketemperatureString: String {
-        return "\(feelsLiketemperature.rounded())"
+        return String(format: "%.0f", feelsLiketemperature)
     }
     let conditionCode: Int
+    var systemIconNameString: String {
+        switch conditionCode {
+        case 200...231:
+            return "cloud.bolt.rain.fill"
+        case 300...321:
+            return "cloud.drizzle.fill"
+        case 500...531:
+            return "cloud.rain.fill"
+        case 600...622:
+            return "cloud.snow.fill"
+        case 701...781:
+            return "cloud.fog.fill"
+        case 800:
+            return "sun.max.fill"
+        case 800...804:
+            return "smoke.fill"
+        default:
+            return "nosign"
+        }
+    }
     
     init?(currentWeatherData: CurrentWeatherData) {
         cityName = currentWeatherData.name
--- Weather/Model/NetworkWetherManager.swift
+++ Weather/Model/NetworkWetherManager.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-struct  NetworkWetherManager {
+class  NetworkWetherManager {
     
     var onCompletion: ((CurrentWeather) -> Void)?
     
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&apikey=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
