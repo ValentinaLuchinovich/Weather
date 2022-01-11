f476e7df8a441d5cc6623c90f597ff6649cc8b9a
ValentinaLuchinovich <valinkontakt11@bk.ru>
2022-01-11 19:16:40 +0300

    Реализована передача данных при помощи клоужера

--- Weather/Extensions/ViewController+alertController.swift
+++ Weather/Extensions/ViewController+alertController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension ViewController {
-    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style, completionHandler: @escaping (String) -> Void) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: style)
         ac.addTextField { tf in
             let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
